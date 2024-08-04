58ff447e03b3302e0aad6071348ac9361f13bd62
EDEC <min20rb@gmail.com>
2024-08-04 16:56:45 +0900

    2ndtry

---
+++ Qdi_MOD_Creatures_proj/Qdi_MOD_Creatures/Qdi_MOD_Creatures/JormungandrWeapon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using WorkerSpine;
+
+namespace Qdi_MOD_Creatures
+{
+    public class JormungandrWeapon : EquipmentScriptBase
+    {
+
+
+    }
+}
